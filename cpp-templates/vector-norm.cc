41fca42d8fbed7a8a039a4748abc42a0d5476c01
Timo Heister <timo.heister@gmail.com>
2023-11-29 08:50:24 -0500

    add c++ examples

---
+++ cpp-templates/vector-norm.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+template<typename number>
+number square(const number x)
+{
+  return x*x;
+}
+
+template<int N>
+struct Point
+{
+  int elements[N];
+};
+
+
+template <int N>
+double norm (const Point<N> &v)
+{
+  double tmp = 0;
+  for (int i=0; i<N; ++i)
+    tmp += square(v.elements[i]);
+  return std::sqrt(tmp);
+}
+
+int main()
+{
+  int y = square<>(5);
+  std::cout << "5*5 = " << y << std::endl;
+
+  Point<2> p;
+  p.elements[0]=1.0;
+  p.elements[1]=4.0;
+
+  std::cout << "norm of p = " << norm (p) << std::endl;
+}
